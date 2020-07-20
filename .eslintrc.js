e63c6615f38ce886ab69e6927c4c7777d89bc2f0
余丽珊 <584221092@qq.com>
2020-07-20 17:29:56 +0800

    配置eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,14 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        node: true
-    },
-    parserOptions: {
-        parser: "babel-eslint"
-    },
-    extends: ["plugin:vue/essential", "@vue/prettier"],
-    plugins: ["vue", "prettier"],
-    rules: {
-        eqeqeq: 0, //
-        "no-console": 0,
-        "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-        "no-new": 0,
-        "no-new-object": 0,
-        "prettier/prettier": [
-            1,
-            {
-                singleQuote: true, //强制单引号
-                trailingComma: "es5", //自动增加逗号
-                printWidth: 150
-            }
-        ],
-
-        "vue/html-indent": 0, //缩进
-        "vue/html-self-closing": 0,
-        "vue/singleline-html-element-content-newline": 0,
-        // 属性排序
-        "vue/attributes-order": 1,
-        "vue/max-attributes-per-line": [
-            0,
-            {
-                singleline: 4
-            }
-        ]
-    }
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ["plugin:vue/essential", "@vue/prettier"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  },
+  parserOptions: {
+    parser: "babel-eslint"
+  }
 };
--- src/views/screenManage/screenEdit/screenEdit.vue
+++ src/views/screenManage/screenEdit/screenEdit.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="screen-edit">
-      2222
+    <div class="header">
+      <div>电视统计大屏</div>
+      <div class="header-center">
+        <ul>
+          <li>
+            <a-icon type="bar-chart" />
+            <p>添加图表</p>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
