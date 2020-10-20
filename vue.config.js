3524c593e805a18db13181a15449abaf77b65353
huangjinxing <1018532015@qq.com>
2020-10-20 14:34:38 +0800

    fix: 修复ico显示问题

--- vue.config.js
+++ vue.config.js
@@ -24,6 +24,15 @@ module.exports = {
 
     }
   },
+  pwa: {
+    iconPaths: {
+        favicon32: 'favicon.ico',
+        favicon16: 'favicon.ico',
+        appleTouchIcon: 'favicon.ico',
+        maskIcon: 'favicon.ico',
+        msTileImage: 'favicon.ico',
+    }
+  },
   productionSourceMap: false,
   lintOnSave: process.env.NODE_ENV !== 'production',
   assetsDir: 'static',
