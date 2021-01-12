2bd9e9ab42a29366e77abab7fb071d226a4bfd0c
huangjinxing <1018532015@qq.com>
2021-01-12 15:34:26 +0800

    refactor(自定义sql): 添加文案说明

--- src/views/dataSource/dataModel/model-edit/setting/sql-setting.vue
+++ src/views/dataSource/dataModel/model-edit/setting/sql-setting.vue
@@ -20,7 +20,9 @@
           <a-input placeholder="请输入视图名称" v-model="form.name"></a-input>
         </a-form-model-item>
         <div>
-          <a-row><a-col :offset="4"><a-icon type="exclamation-circle" />SQL里不能存在同名字段</a-col></a-row>
+          <a-row>
+            <a-col :offset="4"><a-icon type="exclamation-circle" style="margin-right: 2px;" />仅支持ANSI SQL语法 <a-icon type="exclamation-circle" style="margin-right: 2px;" />SQL里不能存在同名字段</a-col>
+          </a-row>
           <a-form-model-item label="SQL语句" prop="cloneSql">
             <a-textarea style="height:200px;resize:unset" v-model="cloneSql" @blur.native.prevent.stop="handleValidateSql" />
           </a-form-model-item>
