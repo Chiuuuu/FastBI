ce518a4499d2ddfdac82842ab676069a67db7f12
zhaowk <ckt5532@dingtalk.com>
2021-03-19 16:30:47 +0800

    perf: 优化分列方法

--- src/views/dataSource/dataAccess/components/data-main/tab-content/structure/table-info.vue
+++ src/views/dataSource/dataAccess/components/data-main/tab-content/structure/table-info.vue
@@ -26,7 +26,7 @@
         <thead>
           <tr>
             <th>序号</th>
-            <th v-for="(item, index) in columns" :key="index" :title="item['COLUMN_NAME']">
+            <th v-for="(item, index) in currentCol" :key="index" :title="item['COLUMN_NAME']">
               {{ item['COLUMN_NAME'] }}
               <span class="type">{{ item['TYPE_NAME'] | formatType }}</span>
             </th>
@@ -36,7 +36,7 @@
           <tr v-for="(item, index) in tableData" :key="index">
             <td>{{ index + 1 }}</td>
             <td
-              v-for="(col, i) in columns"
+              v-for="(col, i) in currentCol"
               :key="i"
               :title="item[col['COLUMN_NAME']]"
             >{{ item[col['COLUMN_NAME']] }}</td>
@@ -60,8 +60,9 @@ export default {
       spinning: false,
       bodyStyle: { height: 'calc(100vh - 240px)', 'overflow-y': 'auto' },
       columns: [],
+      currentCol: [],
       tableData: [],
-      pageList: {},
+      pageList: [],
       colPagination: {
         total: 0,
         size: 50,
@@ -95,31 +96,28 @@ export default {
       } else {
         this.columns = []
         this.tableData = []
-        this.pageList = {}
+        this.pageList = []
       }
     }
   },
   methods: {
     handleGetTableInfo() {
-      this.spinning = true
-      const params = {
-        id: this.tableId,
-        size: this.colPagination.size,
-        page: this.colPagination.page
-      }
-      if (this.pageList[params.page]) {
-        this.spinning = false
-        this.columns = this.pageList[params.page].headers
-        this.tableData = this.pageList[params.page].rows
+      const { size, page } = this.colPagination
+      if (this.validPageSection()) {
+        this.currentCol = this.columns.slice((page - 1) * size, page * size)
       } else {
+        this.spinning = true
+        const params = {
+          id: this.tableId,
+          size: size,
+          page: page
+        }
         this.$server.dataAccess
           .getTableInfo(params)
           .then((res) => {
             if (res.code === 200) {
-              this.pageList[params.page] = res.tableDataInfo
-              this.columns = res.tableDataInfo.headers
-              this.tableData = res.tableDataInfo.rows
               this.colPagination.total = res.total
+              this.handleColCache(res.tableDataInfo)
             } else {
               this.$message.error(res.msg)
             }
@@ -128,6 +126,46 @@ export default {
             this.spinning = false
           })
       }
+    },
+    /**
+     * 考虑到今后有可能有跳页和改变size的情况, 先实现一个方法缓存记录
+     * 设置一个长度为total的数组, 请求的区间缓存到数组, 下请求前做校验
+     */
+    handleColCache(tableDataInfo) {
+      const { size, page, total } = this.colPagination
+      const start = size * (page - 1)
+      const end = size * page > total ? total - 1 : size * page - 1
+      if (this.pageList.length < 1) {
+        this.pageList.length = total
+        this.columns.length = total
+      }
+      // 将当前区间数据写入缓存
+      for (let s = start; s <= end; s++) {
+        this.pageList.splice(s, 1, s)
+        this.columns.splice(s, 1, tableDataInfo.headers[s - start])
+      }
+      if (this.tableData.length > 0) {
+        this.tableData.map((item, index) => {
+          return Object.assign(item, tableDataInfo.rows[index])
+        })
+      } else {
+        this.tableData = tableDataInfo.rows
+      }
+      this.currentCol = this.columns.slice((page - 1) * size, page * size)
+    },
+    // 校验当前缓存区间
+    validPageSection() {
+      const { size, page, total } = this.colPagination
+      const start = size * (page - 1)
+      const end = size * page > total ? total - 1 : size * page - 1
+      if (total < 1) return false
+      // 遍历当前区间是否存在缓存
+      for (let s = start; s <= end; s++) {
+        if (this.pageList[s] === undefined) {
+          return false
+        }
+      }
+      return true
     },
     handleChangePage(type) {
       if (type === 'add') {
