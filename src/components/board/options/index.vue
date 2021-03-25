a45d1c29a5d1f8742dd8ac2b34238c8c167147d8
leizijun <leizijun@zjchilink.com>
2021-03-25 16:16:24 +0800

    fix: 修改字体引用名称去掉中文

--- src/components/board/options/index.vue
+++ src/components/board/options/index.vue
@@ -2072,13 +2072,13 @@ export default {
       fontFamilyList: [
         { label: '默认', value: 'not specified' },
         { label: 'simfang', value: 'simfang' },
-        { label: '仿宋_GB2312', value: '仿宋_GB2312' },
+        { label: '仿宋_GB2312', value: 'fangsong' },
         { label: 'times', value: 'times' },
         { label: '微软雅黑', value: 'msyh' },
         { label: 'simkai', value: 'simkai' },
-        { label: '庞门正道标题体', value: '庞门正道标题体' },
+        { label: '庞门正道标题体', value: 'pangmenzhengdao' },
         { label: 'HuXiaoBoNanShenTi-2', value: 'HuXiaoBoNanShenTi-2' },
-        { label: '优设标题黑', value: '优设标题黑' },
+        { label: '优设标题黑', value: 'youshe' },
         { label: 'digital-7-4', value: 'digital-7-4' }
       ]
     }
