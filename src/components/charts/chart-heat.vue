70221c17dc3e55caf240edf57ec014698a8b6f94
leizijun <leizijun@zjchilink.com>
2021-07-22 14:10:46 +0800

    fix: 修复散点图关联旭日图，散点图显示错乱

--- src/components/charts/chart-heat.vue
+++ src/components/charts/chart-heat.vue
@@ -171,7 +171,7 @@ export default {
             let max = list.map(item => item.value)
             this.option.visualMap.max = Math.max(...max)
             this.option.series.data = [...list]
-            this.$store.dispatch('SetSelfProperty', this.option)
+            // this.$store.dispatch('SetSelfProperty', this.option)
             this.mychart.setOption(this.option)
           } else {
             // 维度
@@ -195,7 +195,7 @@ export default {
               //   this.option.xAxis.data = val.source.rows.map(x=>(x[dim[0]]));
               this.option.visualMap.max = Math.max(...meaarr)
               this.option.series.data = [..._series]
-              this.$store.dispatch('SetSelfProperty', this.option)
+            //   this.$store.dispatch('SetSelfProperty', this.option)
               this.mychart.setOption(this.option)
             }
             // console.clear();
