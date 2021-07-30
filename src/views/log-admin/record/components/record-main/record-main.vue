39cdc1aad8a7f34044ec39fbe04a5e58ee4e373f
maishuting <2585779482@qq.com>
2021-07-30 17:42:45 +0800

    feat: 添加日志管理

---
+++ src/views/log-admin/record/components/record-main/record-main.vue
@@ -0,0 +1,172 @@
+<template>
+  <div class="record-layout">
+    <div class="list-controller">
+      <a-form-model layout="inline" :model="formData" ref="formData">
+        <a-form-model-item :label="dataName+'：'" prop="name">
+          <a-input
+            v-model="formData.name"
+            class="form-item"
+            placeholder="请输入"
+          ></a-input>
+        </a-form-model-item>
+        <a-form-model-item label="操作者：" prop="people">
+          <a-input
+            v-model="formData.people"
+            class="form-item"
+            placeholder="请输入"
+          ></a-input>
+        </a-form-model-item>
+        <a-form-model-item label="账号：" prop="user">
+          <a-input
+            v-model="formData.user"
+            class="form-item"
+            placeholder="请输入"
+          ></a-input>
+        </a-form-model-item>
+        <a-form-model-item label="操作时间：" prop="time">
+          <a-date-picker 
+            v-model="formData.time"
+            class="form-item"
+            placeholder="请选择"
+            valueFormat="YYYY-MM-DD"
+          />
+        </a-form-model-item>
+        <a-form-model-item>
+          <a-button
+            type="primary"
+            @click="() => getList()"
+            :disabled="loading"
+            >查询</a-button
+          >
+        </a-form-model-item>
+        <a-form-model-item>
+          <a-button type="default" @click="resetForm" :disabled="loading"
+            >重置</a-button>
+        </a-form-model-item>
+      </a-form-model>
+    </div>
+    <!-- 列表 -->
+    <div class="list-table scrollbar">
+      <a-table
+        row-key="id"
+        :columns="column"
+        :data-source="dataList"
+        :loading="loading"
+        :pagination="pagination"
+        :scroll="{ y: 'calc(100vh - 398px)', x: 770 }"
+        @change="handleTableChange"
+      >
+      </a-table>
+    </div>
+  </div>
+
+</template>
+
+<script>
+const column = [
+  {
+    title: '项目名称',
+    width: 150,
+    ellipsis: true,
+    dataIndex: 'projectName'
+  },
+  {
+    title: '数据大屏名称',
+    width: 140,
+    ellipsis: true,
+    dataIndex: 'name'
+  },
+  {
+    title: '操作时间',
+    width: 120,
+    ellipsis: true,
+    dataIndex: 'time'
+  },
+  {
+    title: '操作者',
+    dataIndex: 'people'
+  },
+  {
+    title: '账号',
+    dataIndex: 'user',
+    width: 200,
+    ellipsis: true
+  },
+  {
+    title: '操作类型',
+    dataIndex: 'type',
+    width: 200,
+    ellipsis: true
+  },
+  {
+    title: '内容',
+    dataIndex: 'con',
+    width: 200,
+    ellipsis: true
+  },
+]
+export default {
+  props:{
+    // 类型 1数据接入/2数据建模/3数据大屏
+    type:{
+      type:String,
+      require:true
+    }
+  },
+  data(){
+    return{
+      // 搜索
+      formData:{
+        name:'',
+        people:'',
+        user:'',
+        time:'',
+      },
+      loading:false,
+      column,// 表单配置
+      dataList:[],//列表数据
+      pagination: {
+        current: 1,
+        pageSize: 10,
+        total: 0
+      },
+    }
+  },
+  computed:{
+    dataName(){
+      switch(this.type){
+        case '1':
+          return '数据源名称';
+        case '2':
+          return '数据建模名称';
+        case '3':
+          return '数据大屏名称';
+        default:
+          return '--'
+      }
+    }
+  },
+  methods:{
+    // 获取列表数据
+    async getList(){
+      console.log(this.formData)
+    },
+    // 重置
+    resetForm() {
+      this.$refs.formData.resetFields()
+      this.$nextTick(() => {
+        this.getList()
+      })
+    },
+    // 翻页
+    handleTableChange(pagination) {
+      console.log(pagination,'pagination')
+      this.getList()
+    },
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
