10fb2573b7742e19e8cbb6170c08ad354fb296f5
zhaowk <ckt5532@dingtalk.com>
2021-11-30 16:19:00 +0800

    refactor: 暂时屏蔽自定义字段创建地理字段

--- src/components/board/model/index.vue
+++ src/components/board/model/index.vue
@@ -145,6 +145,7 @@
                             >
                             <a-sub-menu
                               key="4"
+                              v-if="item2.tableNo > 0"
                               title="创建地理字段"
                               @click="openGeoSetting(item2)"
                             >
